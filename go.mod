927cec6d09c6086187978c18184ba9363ad6e027
JeremiSavard <jeremi.savard@usherbrooke.ca>
2021-02-20 16:04:04 -0500

    basic auth with keycloak working

--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module github.com/Ubivius/microservice-template
 go 1.15
 
 require (
+	github.com/coreos/go-oidc v2.2.1+incompatible
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/gorilla/mux v1.8.0
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/pquerna/cachecontrol v0.0.0-20201205024021-ac21108117ac // indirect
 	go.opentelemetry.io/otel/exporters/stdout v0.16.0
 	go.opentelemetry.io/otel/sdk v0.16.0
+	golang.org/x/oauth2 v0.0.0-20210216194517-16ff1888fd2e
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"time"
 
 	"github.com/Ubivius/microservice-template/handlers"
+	"github.com/coreos/go-oidc"
 	"github.com/gorilla/mux"
 	"go.opentelemetry.io/otel/exporters/stdout"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"golang.org/x/oauth2"
 )
 
 func main() {
@@ -39,6 +43,116 @@ func main() {
 
 	// Mux route handling with gorilla/mux
 	router := mux.NewRouter()
+
+	//Authentication setup
+	configURL := "http://localhost:8080/auth/realms/ubivius"
+	ctx = context.Background()
+	provider, err := oidc.NewProvider(ctx, configURL)
+	if err != nil {
+		logger.Println("Auth panic")
+		panic(err)
+	}
+
+	clientID := "ubivius-client"
+	clientSecret := "ef14f638-98ad-4c5b-9320-a223077e0797"
+
+	redirectURL := "http://localhost:9090/ubivius/callback"
+	// Configure an OpenID Connect aware OAuth2 client.
+	oauth2Config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		// Discovery returns the OAuth2 endpoints.
+		Endpoint: provider.Endpoint(),
+		// "openid" is a required scope for OpenID Connect flows.
+		Scopes: []string{oidc.ScopeOpenID, "profile", "email"},
+	}
+	state := "somestate"
+
+	oidcConfig := &oidc.Config{
+		ClientID: clientID,
+	}
+	verifier := provider.Verifier(oidcConfig)
+	authRouter := router.Methods(http.MethodGet).Subrouter()
+	//Auth router
+	authRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rawAccessToken := r.Header.Get("Authorization")
+		if rawAccessToken == "" {
+			http.Redirect(w, r, oauth2Config.AuthCodeURL(state), http.StatusFound)
+			return
+		}
+
+		parts := strings.Split(rawAccessToken, " ")
+		if len(parts) != 2 {
+			w.WriteHeader(400)
+			return
+		}
+		_, err := verifier.Verify(ctx, parts[1])
+		logger.Println(err)
+		if err != nil {
+			logger.Println("error redirecting " + oauth2Config.AuthCodeURL(state))
+			http.Redirect(w, r, oauth2Config.AuthCodeURL(state), http.StatusFound)
+			return
+		}
+
+		val, err := w.Write([]byte("hello world"))
+		if err != nil {
+			logger.Println("error writing hello world")
+			http.Error(w, err.Error(), val)
+			return
+		}
+	})
+
+	authRouter.HandleFunc("/ubivius/callback", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("state") != state {
+			logger.Println("state did not match")
+			http.Error(w, "state did not match", http.StatusBadRequest)
+			return
+		}
+
+		oauth2Token, err := oauth2Config.Exchange(ctx, r.URL.Query().Get("code"))
+		if err != nil {
+			logger.Println("Failed to exchange token")
+			http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+		if !ok {
+			logger.Println("No id_token field in oauth2 token.")
+			http.Error(w, "No id_token field in oauth2 token.", http.StatusInternalServerError)
+			return
+		}
+		idToken, err := verifier.Verify(ctx, rawIDToken)
+		if err != nil {
+			logger.Println("Auth Failed to verify ID Token")
+			http.Error(w, "Failed to verify ID Token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		resp := struct {
+			OAuth2Token   *oauth2.Token
+			IDTokenClaims *json.RawMessage // ID Token payload is just JSON.
+		}{oauth2Token, new(json.RawMessage)}
+
+		if err := idToken.Claims(&resp.IDTokenClaims); err != nil {
+			logger.Println("Auth idToken claim error")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		data, err := json.MarshalIndent(resp, "", "    ")
+		if err != nil {
+			logger.Println("Auth marshal indent error")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		val, err := w.Write(data)
+		if err != nil {
+			logger.Println("Auth write data error")
+			http.Error(w, err.Error(), val)
+			return
+		}
+	})
 
 	// Get Router
 	getRouter := router.Methods(http.MethodGet).Subrouter()
